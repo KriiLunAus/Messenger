c7f2ddce6123526a07d217d1af12f28a8df0d8f3
Krii <kriilunaus16@gmail.com>
2025-01-10 09:56:29 +0200

    Setup (index with title bar)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,39 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React, {useContext} from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { ThemeContext } from '@/context/themeContext';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+
+
+  const context = useContext(ThemeContext)
+
+  if (!context) {
+  throw new Error("ThemeContext must be used within a ThemeProvider")
+  }
+  
+  const { theme } = context;
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: {backgroundColor: theme.background},
+        tabBarLabelStyle: {color: theme.text}
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Chats',
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble" size={24} color="white" />,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
+      /><Tabs.Screen
+        name="adsd"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'sasf',
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble" size={24} color="white" />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,81 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, {useContext} from 'react';
+import {  StyleSheet, View, Text, Pressable, StatusBar } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import { ThemeContext } from '@/context/themeContext';
+const index = () => {
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const context = useContext(ThemeContext)
 
-export default function HomeScreen() {
+  if (!context) {
+  throw new Error("ThemeContext must be used within a ThemeProvider")
+  }
+  
+  const { theme, colorScheme } = context;
+
+  const styles = createStyles(theme, colorScheme)
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={styles.container}>
+      <View style={styles.bar}>
+        <View style={styles.barLeft}>
+          <Pressable style={styles.profile}>
+            <Ionicons name="person" size={24} color="white"  />
+          </Pressable>
+          <Text style={styles.title}>Shmignal</Text>
+        </View>
+        <View style={styles.barRight}>
+          <Pressable>
+            <AntDesign name="search1" size={24} color="white" style={{marginRight: 20}} />
+          </Pressable>
+          <Pressable>
+            <Entypo name="dots-three-vertical" size={24} color="white" />
+          </Pressable>
+        </View>
+      </View>
+            <StatusBar barStyle={theme.text} backgroundColor={theme.background} />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default index
+
+
+const createStyles = (theme, colorScheme) => {
+  
+ return StyleSheet.create({
+    container: {
+      backgroundColor: theme.background,
+      minHeight: '110%',
+    },
+    bar: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: 'space-between',
+
+      padding: 10
+    },
+    barLeft: {
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    barRight: {
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    profile: {
+      borderRadius: 12,
+      overflow: "hidden",
+      backgroundColor: "red"
+    },
+    title: {
+      color: "white",
+      fontSize: 24,
+      marginLeft: 20,
+    }
+
+
+  })
+}
